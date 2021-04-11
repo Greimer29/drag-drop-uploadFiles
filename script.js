41c76e90a5df6105b6ce284a8976dc1f0f58b82c
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-11 13:40:39 -0400

    Adding events on the drop-zone

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 input = document.querySelectorAll('.drop-zone__input').forEach(inputElement => {
     const dropZoneElement = inputElement.closest('.drop-zone');
 
-
-})
+    dropZoneElement.addEventListener('dragover', e => {
+        dropZoneElement.classList.add('drop-zone--over');
+    });
+});
