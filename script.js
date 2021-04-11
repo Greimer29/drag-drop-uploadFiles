4a1c0ecdd8acb96cf070778481494509891abc61
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-11 13:41:59 -0400

    Prevent the default event at drop & drag a file at the navigator

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ input = document.querySelectorAll('.drop-zone__input').forEach(inputElement => {
     const dropZoneElement = inputElement.closest('.drop-zone');
 
     dropZoneElement.addEventListener('dragover', e => {
+        e.preventDefault();
         dropZoneElement.classList.add('drop-zone--over');
+
+        ['draend', 'dragleave'].forEach(type => {
+            dropZoneElement.addEventListener(type, e => {
+                dropZoneElement.classList.remove('drop-zone--over');
+            });
+        });
     });
 });
