74b6ce4a0efb1c11b793a6719db7d1a15d825aa9
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-11 15:37:21 -0400

    Adding the click event for pick the file

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 input = document.querySelectorAll('.drop-zone__input').forEach(inputElement => {
     const dropZoneElement = inputElement.closest('.drop-zone');
 
+    dropZoneElement.addEventListener('click', e => {
+        inputElement.click();
+    });
+
+    dropZoneElement.addEventListener('change', e => {
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+    })
+
     dropZoneElement.addEventListener('dragover', e => {
         e.preventDefault();
         dropZoneElement.classList.add('drop-zone--over');
